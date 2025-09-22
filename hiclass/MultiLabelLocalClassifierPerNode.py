12da23bcdf24ebb5b453598200c9ab2b4f85d915
Liam Brydon <62733830+MyCreativityOutlet@users.noreply.github.com>
2025-09-23 11:13:21 +1200

    support for threshold as an alternative to tolerance

--- hiclass/MultiLabelLocalClassifierPerNode.py
+++ hiclass/MultiLabelLocalClassifierPerNode.py
@@ -54,6 +54,7 @@ class MultiLabelLocalClassifierPerNode(BaseEstimator, MultiLabelHierarchicalClas
         local_classifier: BaseEstimator = None,
         binary_policy: str = "siblings",
         tolerance: float = None,
+        threshold: float = None,
         verbose: int = 0,
         edge_list: str = None,
         replace_classifiers: bool = True,
@@ -108,6 +109,7 @@ class MultiLabelLocalClassifierPerNode(BaseEstimator, MultiLabelHierarchicalClas
         )
         self.binary_policy = binary_policy
         self.tolerance = tolerance
+        self.threshold = threshold
 
     def fit(self, X, y, sample_weight=None):
         """
@@ -148,7 +150,7 @@ class MultiLabelLocalClassifierPerNode(BaseEstimator, MultiLabelHierarchicalClas
         # Return the classifier
         return self
 
-    def predict(self, X, tolerance: float = None) -> np.ndarray:
+    def predict(self, X, tolerance: float = None, threshold: float = None) -> np.ndarray:
         r"""
         Predict classes for the given data.
 
@@ -172,7 +174,18 @@ class MultiLabelLocalClassifierPerNode(BaseEstimator, MultiLabelHierarchicalClas
         """
         # Check if fit has been called
         check_is_fitted(self)
-        _tolerance = (tolerance if tolerance is not None else self.tolerance) or 0.0
+        _tolerance = None
+        _threshold = None
+
+        if tolerance:
+            _tolerance = tolerance if tolerance is not None else self.tolerance
+        elif threshold:
+            _threshold = threshold if threshold is not None else self.threshold
+        else:
+            _threshold = 0.5
+
+        if _tolerance and _threshold:
+            self.logger_.warning("Both tolerance and threshold defined, threshold will be used over tolerance")
 
         # Initialize array that holds predictions
         y = [[[]] for _ in range(X.shape[0])]
@@ -213,11 +226,13 @@ class MultiLabelLocalClassifierPerNode(BaseEstimator, MultiLabelHierarchicalClas
                     probabilities[:, row] = classifier.predict_proba(subset_x)[:, positive_index][:, 0]
 
                 # get indices of probabilities that are within tolerance of max
-
-                highest_probabilities = np.max(probabilities, axis=1).reshape(-1, 1)
-                indices_probabilities_within_tolerance = np.argwhere(
-                    np.greater_equal(probabilities, highest_probabilities - _tolerance)
-                )
+                if _threshold:
+                    indices_probabilities_within_tolerance = np.argwhere(probabilities >= _threshold)
+                else:
+                    highest_probabilities = np.max(probabilities, axis=1).reshape(-1, 1)
+                    indices_probabilities_within_tolerance = np.argwhere(
+                        np.greater_equal(probabilities, highest_probabilities - _tolerance)
+                    )
 
                 prediction = [[] for _ in range(subset_x.shape[0])]
                 for row, column in indices_probabilities_within_tolerance:
